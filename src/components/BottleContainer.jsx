042ce83a165779e90847f6e20e6e4284fa118002
Reena-D <reena.dadanatti@gmail.com>
2025-01-25 23:07:45 +0530

    update

--- src/components/BottleContainer.jsx
+++ src/components/BottleContainer.jsx
@@ -1,128 +1,101 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 const BottleContainer = () => {
   return (
-    <div className=''>
-      {/* <div className='bottleContainer-middle'>
-      <div className='mid-circle'>hi hello</div>
-      <button className='header-inquiry'>Inquiry Now</button>
-
-      </div> */}
-
+    <div className="">
       <main class="flex flex-col items-center justify-center">
-    {/* <!-- Gradient Circle --> */}
-    <motion.div
-    initial={{ scale: 0.95 }}
-    animate={{ scale: 1 }}
-    transition={{ duration: 1.2, delay: 1.5, ease: "easeInOut" }}
-     // class="h-80 w-80 border-4 border-transparent bg-gradient-to-r from-cyan-400 to-blue-500 flex items-center justify-center"
-    className="w-[400px] h-[400px] rounded-full bg-gradient-to-b from-cyan-400 to-blue-400 p-1"
-    >
-      <motion.div
-      initial={{ scale: 0.95 }}
-      animate={{ scale: 1 }}
-      transition={{ duration: 1.2, delay: 1.5, ease: "easeInOut" }}
-      // class="w-80 h-80 rounded-full bg-gradient-to-b from-cyan-400 to-blue-400 p-1"
-       className="w-full h-full rounded-full bg-white"></motion.div>
-    </motion.div>
-   
-
-    {/* <!-- Bottle Image --> */}
-    {/* <div className='bottleWrapper'>
-    <motion.img
-    initial={{ y: 0 }}
-    animate={{ y: -150 }}
-    transition={{ duration: 1, ease: "easeInOut" }}
-      src="bottles.png" 
-      alt="Bottle"
-      class="bottle absolute top-[247px] left-[663px]   h-[493px] w-[176px]"
-    />
-    </div> */}
-
-    
+        {/* <!-- Gradient Circle --> */}
+        <motion.div
+          initial={{ scale: 0.95 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 1.2, delay: 1.5, ease: "easeInOut" }}
+          className="w-[400px] h-[400px] rounded-full bg-gradient-to-b from-cyan-400 to-blue-400 p-1"
+        >
+          <motion.div
+            initial={{ scale: 0.95 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 1.2, delay: 1.5, ease: "easeInOut" }}
+            className="w-full h-full rounded-full bg-white"
+          ></motion.div>
+        </motion.div>
 
         <motion.img
-         initial={{ y: 0 }}
-         animate={{ y: [0,-250, -150] }}
-         transition={{ delay:1.2, duration: 7.4, ease: "easeInOut", repeatDelay:2,  }}
-          src="Bottle-Cap.svg" // Replace with your bottle image
+          initial={{ y: 0 }}
+          animate={{ y: [0, -250, -150] }}
+          transition={{
+            delay: 1.2,
+            duration: 7.4,
+            ease: "easeInOut",
+            repeatDelay: 2,
+          }}
+          src="Bottle-Cap.svg"
           alt="Bottle Cap"
-          className="top-96 absolute z-10 " // Offset to show only the cap
+          className="top-96 absolute z-10 "
         />
-     
 
-      {/* Bottle Section */}
-    
-              <motion.img
-               initial={{ y: 0 }}
-               animate={{ y: [0,250, -150] }}
-               transition={{ delay:1.2, duration: 6.4, ease: "easeInOut",repeatDelay:0.5, }}
-                src="Bottle-Down.svg" // Replace with your bottle image
-                alt="Bottle Body"
-                className="top-96 absolute z-10" // Offset to show only the body
-              />
-       
+        {/* Bottle Section */}
 
+        <motion.img
+          initial={{ y: 0 }}
+          animate={{ y: [0, 250, -150] }}
+          transition={{
+            delay: 1.2,
+            duration: 6.4,
+            ease: "easeInOut",
+            repeatDelay: 0.5,
+          }}
+          src="Bottle-down.svg"
+          alt="Bottle Body"
+          className="top-96 absolute z-10"
+        />
 
+        {/* Text */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 2 }}
+          transition={{ duration: 2.2, delay: 2.4, ease: "easeInOut" }}
+          className="absolute flex flex-col justify-center items-center top-80"
+        >
+          <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
+            The Ultimate Companion for Hydration
+          </h1>
+          <p style={{ fontSize: "1rem" }}>
+            We believe in the power of hydration. Our mission is simple yet
+            vital.
+          </p>
+        </motion.div>
 
-    {/* Text */}
-     <motion.div
-        initial={{ opacity: 0, scale: 0  
-           }}
-        animate={{ opacity: 1, scale: 2 }}
-        transition={{ duration: 2.2, delay: 2.4,  ease: "easeInOut" }}
-        className='absolute flex flex-col justify-center items-center top-80'
-       
-      >
-        <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
-          The Ultimate Companion for Hydration
-        </h1>
-        <p style={{ fontSize: "1rem" }}>
-          We believe in the power of hydration. Our mission is simple yet vital.
-        </p>
-       
-      </motion.div>
-
-      <motion.button
+        <motion.button
           initial={{ y: 300 }}
           animate={{ y: 100 }}
-          transition={{ duration: 2.2, delay: 2.4, }}
-          className='text-white text-xl w-40 h-12 rounded-full bg-gradient-to-b from-cyan-400 to-blue-400 p-1 '
+          transition={{ duration: 2.2, delay: 2.4 }}
+          className="text-white text-xl w-40 h-12 rounded-full bg-gradient-to-b from-cyan-400 to-blue-400 p-1 "
         >
           Inquiry Now
         </motion.button>
-{/* 
-    <!-- Button Below Circle -->
-    <button
-      class="mt-6 bg-gradient-to-r from-cyan-400 to-blue-500 text-white py-2 px-6 rounded-full shadow-md hover:opacity-90"
-    >
-      Inquiry Now
-    </button>  */}
-
-
- </main>
-<div className='h-32 flex justify-between m-4'>
- <motion.img
-         initial={{ y: 150 }}
-         animate={{ y: -950 }}
-         transition={{ delay:4, duration: 2.4, ease: "easeInOut" }}
-          src="leftBottle.svg" // Replace with your bottle image
+      </main>
+      <div className="h-32 flex justify-between m-4">
+        <motion.img
+          initial={{ y: 150 }}
+          animate={{ y: -950 }}
+          transition={{ delay: 4, duration: 2.4, ease: "easeInOut" }}
+          src="leftBottle.svg"
           alt="Left Bottle"
-          className="" // Offset to show only the cap
+          className=""
         />
 
-<motion.img
-         initial={{ y: 150 }}
-         animate={{ y: -950 }}
-         transition={{ delay:4, duration: 2.4, ease: "easeInOut" }}
-          src="rightBottle.svg" // Replace with your bottle image
+        <motion.img
+          initial={{ y: 150 }}
+          animate={{ y: -950 }}
+          transition={{ delay: 4, duration: 2.4, ease: "easeInOut" }}
+          src="rightBottle.svg"
           alt="Right Bottle"
-          className="bottom-0 left-0" // Offset to show only the cap
+          className="bottom-0 left-0"
         />
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BottleContainer
+export default BottleContainer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,27 @@
-
 import BottleContainer from "@/components/BottleContainer";
 import BottleTypes from "@/components/BottleTypes";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HydrationEssentials from "@/components/HydrationEssentials";
 import Timeline from "@/components/Timeline";
-import Timeline1 from "@/components/Timeline1";
 
-export default function Home () {
+export default function Home() {
   return (
-     <div>
-       <Header/>
-      
-       <BottleContainer/>
-       <BottleTypes/>
-   
-       <div style={{ marginTop: '200px' }}> {/* Adjust the value as needed */}
-    <HydrationEssentials />
-  </div>
-  <div className=" bg-white">
-      <Timeline1 />
+    <div>
+      <Header />
+
+      <BottleContainer />
+      <BottleTypes />
+
+      <div style={{ marginTop: "200px" }}>
+        <HydrationEssentials />
+      </div>
+      <div className=" bg-white">
+        <Timeline />
+      </div>
+      <div style={{ marginTop: "520px" }}>
+        <Footer />
+      </div>
     </div>
-  <div style={{ marginTop: '520px' }}> {/* Adjust the value as needed */}
-    <Footer />
-  </div>
-     </div>
-  )
+  );
 }
